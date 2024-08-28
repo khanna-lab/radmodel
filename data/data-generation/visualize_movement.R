ddaa17ac852b5795a9add7f0a61d3cf56be1f002
Aditya Khanna <khanna7.work@gmail.com>
2024-08-28 17:01:30 -0400

    plot movement of 10 residents (but y-axis is unclear)

--- data/data-generation/visualize_movement.R
+++ data/data-generation/visualize_movement.R
@@ -6,7 +6,7 @@ library(dplyr)
 schedules <- read.csv("schedule.csv")
 
 # Select a few residents to visualize (e.g., first 3)
-selected_residents <- schedules %>% filter(resident_id %in% c(0: 9))
+selected_residents <- schedules %>% filter(resident_id %in% c(0:9))
 
 # Remove long vertical bars by focusing only on movements
 filtered_schedules <- selected_residents %>%
