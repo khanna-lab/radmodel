a254b51bedb6b1fdc15f2e63465ca0fe95e2cc8b
Aditya Khanna <aditya_khanna@brown.edu>
2025-03-24 16:03:46 -0400

    add code for cafeteria

--- analysis/movement-tracker.py
+++ analysis/movement-tracker.py
@@ -64,7 +64,31 @@ plt.tight_layout()
 plt.savefig(Path("analysis", "gym_occupancy.png", dpi=300))
 
 
-## yard
+## cafeteria
 ### data
-gym_counts = merged_movement_places[merged_movement_places["type"] == "gym"]
-print(gym_counts.head())
+cafeteria_counts = merged_movement_places[merged_movement_places["type"] == "cafeteria"]
+print(cafeteria_counts.head())
+
+### plot
+plt.figure(figsize=(10, 5))
+for cafeteria_name, group in cafeteria_counts.groupby("name"):
+    plt.plot(group["tick"], group["person_count"], label=cafeteria_name)
+
+hour_ticks = list(range(0, 96, 4))  
+
+hour_labels = [f"{(h % 12) or 12} {'AM' if h < 12 else 'PM'}" for h in range(24)]
+print(hour_labels)
+
+plt.xticks(hour_ticks, hour_labels, rotation=45)
+
+
+plt.xlim(0, 96)
+plt.xlabel("Time of Day")
+plt.ylabel("People in cafeteria")
+plt.title("cafeteria Occupancy – First Day Only")
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+
+
+plt.savefig(Path("analysis", "cafeteria_occupancy.png", dpi=300))
