24623654bdca1ee257185b7c449c7e5fdafb6dc8
Aditya Khanna <aditya_khanna@brown.edu>
2025-03-24 16:00:03 -0400

    label xaxis in terms of time of day

--- analysis/movement-tracker.py
+++ analysis/movement-tracker.py
@@ -33,15 +33,38 @@ print(merged_movement_places.head(), "\n")
 # Visualize movement by space ---------
 
 ## gym
+
+### data
 gym_counts = merged_movement_places[merged_movement_places["type"] == "gym"]
 print(gym_counts.head())
 
-## plot
 
-
+### plot
 plt.figure(figsize=(10, 5))
 for gym_name, group in gym_counts.groupby("name"):
     plt.plot(group["tick"], group["person_count"], label=gym_name)
-plt.show()
+
+hour_ticks = list(range(0, 96, 4))  
+
+hour_labels = [f"{(h % 12) or 12} {'AM' if h < 12 else 'PM'}" for h in range(24)]
+print(hour_labels)
+
+plt.xticks(hour_ticks, hour_labels, rotation=45)
+
+
+plt.xlim(0, 96)
+plt.xlabel("Time of Day")
+plt.ylabel("People in Gym")
+plt.title("Gym Occupancy – First Day Only")
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+
+#plt.show()
 plt.savefig(Path("analysis", "gym_occupancy.png", dpi=300))
 
+
+## yard
+### data
+gym_counts = merged_movement_places[merged_movement_places["type"] == "gym"]
+print(gym_counts.head())
