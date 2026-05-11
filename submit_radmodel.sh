8a1df37f5d282546a4f9e7ebac6fa721c2c5b2fd
Aditya Khanna <aditya_khanna@brown.edu>
2026-05-11 12:11:33 -0400

    include output directories in the submit radmodel

--- submit_radmodel.sh
+++ submit_radmodel.sh
@@ -19,10 +19,14 @@ mkdir -p logs
 
 source settings.sh
 
+RUN_ID="${SLURM_JOB_ID:-local_$(date +%Y%m%dT%H%M%S)}"
+OUT_DIR="output/run_${RUN_ID}"
+
 echo "Host: $(hostname)"
 echo "Job: ${SLURM_JOB_ID:-<none>}  Params: $PARAMS"
+echo "Output dir: $OUT_DIR"
 echo "Started: $(date -Is)"
 
-mpirun -n "$SLURM_NTASKS" radmodel "$PARAMS"
+mpirun -n "$SLURM_NTASKS" radmodel "$PARAMS" "{\"output_dir\": \"$OUT_DIR\"}"
 
 echo "Finished: $(date -Is)"
